bb8edff92ef11c00d22fee8ffdaf308fa0b8699c
bullark <140177587+bullark@users.noreply.github.com>
2023-07-21 16:57:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,81 +1,52 @@
-if(closeCount > openCount){//bulls count
-      bullCount++;
-      //checking for R.L.P
-      if(high - close < candleSize * 0.1){//GREEN hammer
-         if(open - low > candleSize * 0.6){
-            RLPCount++;
-            if(candleIndex > 11){
-               if(bullCount > bearCount && RLPCount > RHPCount){//the bulls are many
-                  Comment("green candles --- more bulls --- more R.L.P.");
-               }//the bulls are many
-               if(bearCount > bullCount && RHPCount > RLPCount){//the bear are many
-                  Comment("green candles --- more bears --- more R.H.P.");
-               }//the bear are many
-            }
-         }
-      }//checking for R.L.P  
-   }//bulls count
-   
-   if(closeCount < openCount){//bears count
-      bearCount++;
-      //checking for R.H.P
-      if(high - close > candleSize * 0.6){//green shooting-star
-         if(open - low < candleSize * 0.1){
-            RHPCount++;
-            if(candleIndex > 11){
-               if(bullCount < bearCount && RLPCount < RHPCount){//the bulls are many
-                  Comment("red candles --- more bears --- more R.H.P.");
-               }//the bulls are many
-               if(bearCount < bullCount && RHPCount < RLPCount){//the bear are many
-                  Comment("red candles --- more bears --- more R.H.P.");
-               }//the bear are many
-            }
-         }
-      }//checking for R.H.P
-   }//bears count
-   
-   
-///////////////ANOTHER ONE ///////////////////////////////////////
+// Get all input elements
+const incomeAmountInput = document.getElementById("incomeAmount");
+const savingsPercentageInput = document.getElementById("savingsPercentage");
+const emergencyPercentageInput = document.getElementById("emergencyPercentage");
+const housingAmountInput = document.getElementById("housingAmount");
+const dailyUseInput = document.getElementById("dailyUse");
+const utilitiesInput = document.getElementById("utilities");
+const transportationInput = document.getElementById("transportation");
+const othersInput = document.getElementById("others");
 
+// Get all summary elements
+const incomeTotalElement = document.getElementById("incomeTotal");
+const expenditureTotalElement = document.getElementById("expenditureTotal");
+const savingsTotalElement = document.getElementById("savingsTotal");
+const emergencyTotalElement = document.getElementById("emergencyTotal");
+const remainingIncomeElement = document.getElementById("remainingIncome");
 
-if(candleIndex < 60 && bullCount > bearCount){//looking of buy signals
-      if(highCount > highCount3 && highCount3 > highCount5){
-         if(MainLine[1] > 0){//when signal ismain above 0
-             if(MainLine[1] > Signal[1] && MainLine[3] < Signal[3]){
-                  if(MainLine[1] > MainLine[3] && MainLine[3] > MainLine[5]){
-                        Comment("my Uptrend");
-                        createOBJ(time, low, 233, clrGreen, "Buy");
-                  }
-             }
-         }//when signal is below 0 and main above 0
-         if(MainLine[1] < 0){//when below above 0
-            if(-MainLine[1] < -Signal[1] && -MainLine[3] > -Signal[3]){
-               if(MainLine[1] > MainLine[3] && MainLine[3] > MainLine[5]){
-                  Comment("my Uptrend");
-                  createOBJ(time, low, 233, clrGreen, "Buy");
-               }
-            }
-         }//when below above 0
-      }
-   }//looking of buy signals
-   
-   if(candleIndex < 60 && bearCount > bullCount){//looking of sell signals
-      if(lowCount < lowCount3 && lowCount3 < lowCount5){
-        if(MainLine[1] < 0){//when main is above 0
-           if(-MainLine[1] > -Signal[1] && -MainLine[3] < -Signal[3]){
-               if(-MainLine[1] > -MainLine[3] && -MainLine[3] > -MainLine[5]){
-                  Comment("Downtrend");
-                  createOBJ(time, high, 234, clrRed, "sell");
-               }
-           }
-         }//when main is above 0
-         if(MainLine[1] > 0){//when main above 0
-            if(MainLine[1] < Signal[1] && MainLine[3] > Signal[3]){
-               if(-MainLine[1] > -MainLine[3] && -MainLine[3] > -MainLine[5]){
-                  Comment("my Downtrend");
-                  createOBJ(time, high, 234, clrRed, "sell");
-               }
-            }
-         }//when main above 0
-      }
-   }//looking of sell signals
+// Function to calculate and update the summary
+function updateSummary() {
+  const incomeAmount = parseFloat(incomeAmountInput.value) || 0;
+  const savingsPercentage = (parseFloat(savingsPercentageInput.value) || 0) / 100;
+  const emergencyPercentage = (parseFloat(emergencyPercentageInput.value) || 0) / 100;
+  const housingAmount = parseFloat(housingAmountInput.value) || 0;
+  const dailyUse = parseFloat(dailyUseInput.value) || 0;
+  const utilities = parseFloat(utilitiesInput.value) || 0;
+  const transportation = parseFloat(transportationInput.value) || 0;
+  const others = parseFloat(othersInput.value) || 0;
+
+  const savingsTotal = incomeAmount * savingsPercentage;
+  const emergencyTotal = incomeAmount * emergencyPercentage;
+  const expenditureTotal = housingAmount + dailyUse + utilities + transportation + others;
+  const remainingIncome = incomeAmount - savingsTotal - emergencyTotal - expenditureTotal;
+
+  incomeTotalElement.textContent = incomeAmount.toFixed(2);
+  expenditureTotalElement.textContent = expenditureTotal.toFixed(2);
+  savingsTotalElement.textContent = savingsTotal.toFixed(2);
+  emergencyTotalElement.textContent = emergencyTotal.toFixed(2);
+  remainingIncomeElement.textContent = remainingIncome.toFixed(2);
+}
+
+// Add event listeners to inputs to update the summary on change
+incomeAmountInput.addEventListener("change", updateSummary);
+savingsPercentageInput.addEventListener("change", updateSummary);
+emergencyPercentageInput.addEventListener("change", updateSummary);
+housingAmountInput.addEventListener("change", updateSummary);
+dailyUseInput.addEventListener("change", updateSummary);
+utilitiesInput.addEventListener("change", updateSummary);
+transportationInput.addEventListener("change", updateSummary);
+othersInput.addEventListener("change", updateSummary);
+
+// Initial update of the summary
+updateSummary();
